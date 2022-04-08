840ec6efcc28670072d980520fa5174aa8ac619a
diegohp <diego.blake11@gmail.com>
2022-04-07 21:42:22 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		path: path.resolve(__dirname, 'dis'),
+		path: path.resolve(__dirname, '/dis'),
 		filename: 'bundle.js',
 		publicPath: '/',
 	},
