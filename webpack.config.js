21145adecafe91b83f23158981e1c04705630674
diegohp <diego.blake11@gmail.com>
2022-04-07 22:23:22 -0500

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'bundle.js',
 		publicPath: '/',
 	},
@@ -59,7 +59,7 @@ module.exports = {
 		}),
 	],
 	devServer: {
-		static: path.join(__dirname, 'dist'),
+		static: path.join(__dirname, 'build'),
 		compress: true,
 		//Historia de lo que sucede en el navegador
 		historyApiFallback: true,
