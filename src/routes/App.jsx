9cf2a6e50c4c90a80ec23f8338830ca7688b25e2
diegohp <diego.blake11@gmail.com>
2022-04-07 23:01:48 -0500

    update browserRouter

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ function App() {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter basename='/shop-with-react'>
+			<BrowserRouter basename='/.'>
 				<Layout>
 					<Routes>
 						<Route exact path='/' element={<Home />} />
