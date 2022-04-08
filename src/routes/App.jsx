5b995d58d8eb434e867356c50062654a8790c7e6
diegohp <diego.blake11@gmail.com>
2022-04-07 23:06:59 -0500

    no basename

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ function App() {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter basename='/.'>
+			<BrowserRouter>
 				<Layout>
 					<Routes>
 						<Route exact path='/' element={<Home />} />
